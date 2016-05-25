5324f94bc412d808ee65f32c14b2c86cc3187852
chenhongxin <chenhongxin@snaillove.com>
2016-05-25 16:29:02 +0800

    first commit

---
+++ src/main/java/com/snaillove/test/test_mvn/App.java
@@ -0,0 +1,15 @@
+package com.snaillove.test.test_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+    }
+}
